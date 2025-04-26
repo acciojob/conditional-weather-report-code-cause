9d0fe1f585ebcb3bb9bb7a49c7325483f4413256
Ashish <writeashish22@gmail.com>
2025-04-27 01:15:44 +0530

    initial3

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,14 @@
-
 import React, { useState } from "react";
 import DisplayWeather from "./DisplayWeather";
 
-
 const App = () => {
-
-const [weather, setWeather] = useState({temperature:25, conditions: 'Sunny'})
+  const [weather, setWeather] = useState({ temperature: 25, conditions: "Sunny" });
 
   return (
     <div>
-     <DisplayWeather  weather={weather} />
-       
+      <DisplayWeather weather={weather} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -1,23 +1,22 @@
-import React,{useState} from 'react'
+import React from "react";
 
-const DisplayWeather = ({weather})=>{
+const DisplayWeather = ({ weather }) => {
+  const { temperature, conditions } = weather;
 
-const {temperature, conditions} = weather;
+  const tempStyles = {
+    color: temperature > 20 ? "red" : "blue"   // ✅ Correct logic (red for >20)
+  };
 
-    const tempStyles ={
-        color: temperature>20 ? 'red' : 'blue'
-    };
-    return(
-        <div>
-
-        <p>
-            Temperature: <span style={tempStyles}>{temperature}</span>
-        </p>
-        <p>{conditions}</p>
-
-        </div>
-    )
-
-}
+  return (
+    <div>
+      <p>
+        Temperature: <span style={tempStyles}>{temperature}</span>
+      </p>
+      <p>
+        Conditions: {conditions}
+      </p>
+    </div>
+  );
+};
 
 export default DisplayWeather;
